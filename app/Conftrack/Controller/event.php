e9674e7d08adbca7b7e27d99b2555887ba71926d
Chris Cornutt <enygma@phpdeveloper.org>
2015-12-10 17:34:32 -0600

    updates to teh event and sponsor controllers

--- app/Conftrack/Controller/event.php
+++ app/Conftrack/Controller/event.php
@@ -2,6 +2,14 @@
 
 $app->group('/event', function() use ($app) {
 
+  $app->get('', function($request, $response, $args) {
+    $events = new \Conftrack\Collection\Events($this->getContainer()->get('db'));
+    $events->findAll();
+
+    $data = ['events' => $events->toArray(true)];
+    $this->view->render($response, 'event/index.twig', $data);
+  });
+
   $app->get('/create', function($request, $response, $args) {
     $sponsors = new \Conftrack\Collection\Sponsors($this->getContainer()->get('db'));
     $sponsors->findAll();
@@ -10,6 +18,64 @@ $app->group('/event', function() use ($app) {
       'sponsors' => $sponsors->toArray(true)
     ];
     $this->view->render($response, 'event/create.twig', $data);
+  });
+
+  $app->get('/view/{eventId}', function($request, $response, $args) {
+    $event = new \Conftrack\Model\Event($this->getContainer()->get('db'));
+    $event->findById($args['eventId']);
+
+    $sponsors = new \Conftrack\Collection\Sponsors($this->getContainer()->get('db'));
+    $sponsors->findAll();
+
+    $data = [
+      'event' => $event->toArray(),
+      'sponsors' => $sponsors->toArray(true),
+      'eventSponsors' => $event->sponsors->toArray(true)
+    ];
+    $this->view->render($response, 'event/view.twig', $data);
+  });
+
+  $app->post('/{eventId}/sponsor', function($request, $response, $args) {
+    // Add a sponsor to the event
+    $data = ['success' => false];
+    $body = $request->getParsedBody();
+
+    $sponsorEvent = new \Conftrack\Model\SponsorEvent($this->getContainer()->get('db'));
+    $sponsorEvent->load([
+      'event_id' => $args['eventId'],
+      'sponsor_id' => $body['sponsorId']
+    ]);
+
+    try {
+        $sponsorEvent->verify();
+        $sponsorEvent->save();
+        $data['success'] = true;
+        $data['message'] = 'Sponsor added successfully';
+
+    } catch (\Exception $e) {
+      $data['message'] = "Error: ".implode("\n", $sponsorEvent->getMessages());
+    }
+
+    return $response->withJson($data);
+  });
+
+  $app->delete('/{eventId}/sponsor', function($request, $response, $args) {
+    $data = ['success' => false];
+    $body = $request->getParsedBody();
+
+    $sponsorEvent = new \Conftrack\Model\SponsorEvent($this->getContainer()->get('db'));
+    $sponsorEvent->find([
+      'sponsor_id' => $body['sponsorId'],
+      'event_id' => $args['eventId']
+    ]);
+    try {
+        $data['success'] = $sponsorEvent->delete();
+        $data['message'] = 'Sponsor removed successfully';
+    } catch (\Exception $e) {
+      $data['message'] = "Error: ".implode("\n", $sponsorEvent->getMessages());
+    }
+
+    return $response->withJson($data);
   });
 
   $app->post('/create', function($request, $response, $args) {
--- app/Conftrack/Controller/sponsor.php
+++ app/Conftrack/Controller/sponsor.php
@@ -5,11 +5,11 @@ $app->group('/sponsor', function() use ($app) {
   $app->get('', function($request, $response, $args) {
     $sponsors = new \Conftrack\Collection\Sponsors($this->getContainer()->get('db'));
     $sponsors->findAll();
-    
+
     $data = [
       'sponsors' => $sponsors->toArray(true)
     ];
-    $this->view->render($response, 'sponsor/view.twig', $data);
+    $this->view->render($response, 'sponsor/index.twig', $data);
   });
 
   $app->get('/view/{sponsorId}', function($request, $response, $args) {
